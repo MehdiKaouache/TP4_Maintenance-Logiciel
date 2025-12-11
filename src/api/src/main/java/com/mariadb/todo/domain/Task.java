f98097c91417ccaae9523017e1e99085d8469a90
Mehdi Kaouache <kaouache.mehdi@gmail.com>
2025-12-11 10:34:51 -0500

    Push final du derniere pipeline

--- src/api/src/main/java/com/mariadb/todo/domain/Task.java
+++ src/api/src/main/java/com/mariadb/todo/domain/Task.java
@@ -13,4 +13,28 @@ public class Task {
     private Integer id;
     private String description;
     private Boolean completed = false;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
 }
